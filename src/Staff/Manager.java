cb335c8ca592e610cf02af58911607744eb20efe
Oryngali Dauir <123401433+DauirEZ@users.noreply.github.com>
2023-12-14 23:12:18 +0600

    UpdateManager

--- src/Staff/Manager.java
+++ src/Staff/Manager.java
@@ -1,2 +1,47 @@
-package Staff;public class Manager {
+package Staff;
+
+import java.util.Collections;
+import java.util.List;
+import Students.Student;
+
+public class Manager extends Employee implements StudentOrganization{
+
+    public Manager(){
+        super();
+
+    }
+
+    public Manager(String name, String surname, Double salary){
+        super(name, surname, salary);
+
+    }
+
+    public void printStudentsSortedByGPA(List<Student> students) {
+        Collections.sort(students);
+        System.out.println("Students sorted by GPA:");
+        for (Student student : students) {
+            System.out.println(student.getName() + "'s GPA is "+ student.getGPA());
+        }
+    }
+
+
+    @Override
+    public void createStudentOrganization() {
+
+    }
+
+    @Override
+    public void kickMember() {
+
+    }
+
+    @Override
+    public void acceptMember() {
+
+    }
+
+    @Override
+    public void OrganizeEvent() {
+
+    }
 }
